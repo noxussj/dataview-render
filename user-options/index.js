701a8bdfae51b0d5f15ea74b916abe301dbef85c
HN <632922356@qq.com>
2022-05-23 15:58:22 +0800

    update

--- user-options/index.js
+++ user-options/index.js
@@ -1,8 +1,8 @@
-const { data1, data2, data3, data4, data5, data6 } = require('../user-data/index.js')
+const { data1, data2, data3, data4, data5, data6, data7, data8 } = require('../user-data/index.js')
 
 module.exports = {
-    name: '农机作业',
-    key: 'subsidy',
+    name: '农机普查',
+    key: 'survey',
     prefix: 'overview/machine/',
     layout: [
         {
@@ -17,18 +17,25 @@ module.exports = {
                     copyData: data1
                 },
                 {
-                    name: '地区补贴金额统计',
-                    key: 'region-amount',
-                    height: '48%',
-                    copy: 'barx-gradual',
+                    name: '当前使用状态统计',
+                    key: 'vehicle-status',
+                    height: '30%',
+                    copy: '',
                     copyData: data2
                 },
                 {
-                    name: '农机类型补贴统计',
+                    name: '农机类型统计',
                     key: 'machine-type',
-                    height: '52%',
+                    height: '34%',
                     copy: 'barx-gradual',
                     copyData: data3
+                },
+                {
+                    name: '年作业量农机统计',
+                    key: 'machine-use',
+                    height: '36%',
+                    copy: 'barx-gradual',
+                    copyData: data4
                 }
             ]
         },
@@ -46,21 +53,28 @@ module.exports = {
                     key: 'count2',
                     height: '0%',
                     copy: 'card-count2',
-                    copyData: data4
-                },
-                {
-                    name: '作业任务统计',
-                    key: 'job-task',
-                    height: '48%',
-                    copy: 'barx-gradual',
                     copyData: data5
                 },
                 {
-                    name: '补贴进度统计',
-                    key: 'progress-count',
-                    height: '52%',
+                    name: '新疆农机数量分布统计',
+                    key: 'machine-dist',
+                    height: '35%',
                     copy: 'barx-gradual',
                     copyData: data6
+                },
+                {
+                    name: '农机厂家占比分析',
+                    key: 'factory-dist',
+                    height: '30%',
+                    copy: 'pie-rose',
+                    copyData: data7
+                },
+                {
+                    name: '总动力/配套总动力统计',
+                    key: 'total-power',
+                    height: '35%',
+                    copy: 'line-simple',
+                    copyData: data8
                 }
             ]
         }

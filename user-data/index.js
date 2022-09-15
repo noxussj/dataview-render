c44e57edf1ab7de5ff7ba0c821d2283f06c78ed5
HN <632922356@qq.com>
2022-09-15 16:08:40 +0800

    update

--- user-data/index.js
+++ user-data/index.js
@@ -1,210 +1,139 @@
-// 气象站
-const data1 = [
-    {
-        name: '温度',
-        value: '30.5',
-        unit: '­°C',
-        reference: '25.5-28.5­°C',
-        key: 'temperature'
-    },
-    {
-        name: '湿度',
-        value: '70.8',
-        unit: '%',
-        reference: '60.5-80.5%',
-        key: 'humidity'
-    },
-    {
-        name: '光照强度',
-        value: '15.0',
-        unit: 'WLux',
-        reference: '10.0-20.0WLux',
-        key: 'beam'
-    },
-    {
-        name: '风速',
-        value: '2',
-        unit: 'm/s',
-        reference: '0-8m/s',
-        key: 'windSpeed'
-    },
-    {
-        name: '大气压',
-        value: '1210',
-        unit: 'hPa',
-        reference: '300-1250hPa',
-        key: 'pressure'
-    },
-    {
-        name: '降雨量',
-        value: '22',
-        unit: 'mm/h',
-        reference: '0-22mm/s',
-        key: 'rainfall'
-    }
-]
-
-// 土壤监测
-const data2 = [
-    {
-        name: '土壤温度',
-        value: '27.1',
-        unit: '­°C',
-        reference: '25.5-28.5­°C',
-        key: 'humidity'
-    },
-    {
-        name: '土壤湿度',
-        value: '20',
-        unit: '­%',
-        reference: '60.5-80.5%',
-        key: 'humidity'
-    },
-    {
-        name: '土壤EC值',
-        value: '1.889',
-        unit: 'ds/m',
-        reference: '0-1.999ds/m',
-        key: 'ec'
-    }
-]
-
-// 本月桑园虫情预警
-const data3 = {
-    count: {
-        count: '123', // 预警总数
-        catch: '2134', // 捕捉量
-        warningtype: '桑瘿蚊、桑螟、桑粉虱', // 预警总数
-        warningCount: '5' // 预警类型统计
-    },
-    echarts: [
-        {
-            name: '桑粉虱',
-            value: '15'
-        },
-        {
-            name: '桑螟',
-            value: '132'
-        },
-        {
-            name: '桑瘿蚊',
-            value: '46'
-        },
-        {
-            name: '其他',
-            value: '12'
-        }
-    ]
-}
-
-// 24小时孢子捕捉
-const data4 = {
-    content: [
-        {
-            name: '采集流量',
-            value: '0.5',
-            unit: 'm/s'
-        },
-        {
-            name: '采集时间',
-            value: '1-24',
-            unit: '小时'
-        },
-        {
-            name: '抽气时间',
-            value: '1-24',
-            unit: '小时'
-        },
-        {
-            name: '培养仓温设定',
-            value: '25 ',
-            unit: '°C'
-        }
-    ],
-    images: [
-        {
-            name: '桑园捕捉记录1',
-            url: ''
-        },
-        {
-            name: '桑园捕捉记录2',
-            url: ''
-        },
-        {
-            name: '桑园捕捉记录3',
-            url: ''
-        }
-    ]
-}
-
-// 设备监测
-const data5 = {
+// 小程序实时用户统计
+export const data1 = {
+    updateDateTime: '2022-08-12 14:27:23',
     count: [
         {
-            name: '全部',
-            value: '324'
+            name: '访问人数',
+            value: 321,
+            unit: '人',
+            lastDay: 233,
+            lastWeek: -1.1,
+            key: 'visit'
         },
         {
-            name: '蚕房监测设备',
-            value: '123'
+            name: '新用户数',
+            value: 3456,
+            unit: '人',
+            lastDay: 233,
+            lastWeek: -1.1,
+            key: 'new'
         },
         {
-            name: '监控摄像',
-            value: '123'
-        }
-    ],
-    table: [
-        {
-            name: 'XXX设备',
-            value: '1',
-            label: '正常'
-        },
-        {
-            name: 'XXX设备',
-            value: '2',
-            label: '离线'
-        },
-        {
-            name: 'XXX设备',
-            value: '3',
-            label: '未连接'
-        },
-        {
-            name: 'XXX设备',
-            value: '1',
-            label: '正常'
-        },
-        {
-            name: 'XXX设备',
-            value: '2',
-            label: '离线'
-        },
-        {
-            name: 'XXX设备',
-            value: '3',
-            label: '未连接'
-        },
-        {
-            name: 'XXX设备',
-            value: '1',
-            label: '正常'
-        },
-        {
-            name: 'XXX设备',
-            value: '2',
-            label: '离线'
-        },
-        {
-            name: 'XXX设备',
-            value: '3',
-            label: '未连接'
-        },
-        {
-            name: 'XXX设备',
-            value: '3',
-            label: '未连接'
+            name: '累积用户',
+            value: 3456,
+            unit: '万人',
+            lastDay: 233,
+            lastWeek: -1.1,
+            key: 'accumulation'
         }
     ]
 }
 
-module.exports = { data1, data2, data3, data4, data5 }
+// 趋势分析
+export const data2 = {
+    series: [
+        {
+            name: '访问次数（次）',
+            data: [100, 200, 300, 400, 500, 600, 700]
+        },
+        {
+            name: '访问人数（人）',
+            data: [100, 400, 500, 600, 200, 300, 700]
+        },
+        {
+            name: '新增用户（个）',
+            data: [100, 400, 500, 600, 200, 300, 700]
+        }
+    ],
+    axis: ['08-01', '08-02', '08-03', '08-04', '08-05', '08-06', '08-07']
+}
+
+// 专家咨询服务统计
+export const data3 = [
+    {
+        name: '服务用户',
+        value: 3452,
+        unit: '个'
+    },
+    {
+        name: '覆盖率',
+        value: 92,
+        unit: '%'
+    }
+]
+
+// 咨询关键词统计TOP5
+export const data4 = {
+    series: [18, 17, 15, 13, 2],
+    axis: ['环境监测', '饲育技术', '消毒', '预防化肥', '施肥']
+}
+
+// 资询趋势分析
+export const data5 = {
+    series: [
+        {
+            name: '咨询量（次）',
+            data: [100, 200, 300, 400, 500, 600, 700]
+        },
+        {
+            name: '回复量（次）',
+            data: [100, 400, 500, 600, 200, 300, 700]
+        }
+    ],
+    axis: ['08-01', '08-02', '08-03', '08-04', '08-05', '08-06', '08-07']
+}
+
+// 用户反馈分析
+export const data6 = [
+    {
+        name: '觉得有用',
+        value: 46
+    },
+    {
+        name: '觉得没用',
+        value: 132
+    }
+]
+
+// 政策资讯统计
+export const data7 = {
+    count: 123,
+    echarts: [
+        {
+            name: '省级政策',
+            value: 15,
+            unit: '篇'
+        },
+        {
+            name: '市级政策',
+            value: 46,
+            unit: '篇'
+        },
+        {
+            name: '县级政策',
+            value: 132,
+            unit: '篇'
+        }
+    ]
+}
+
+// 技术教程统计
+export const data8 = [
+    {
+        name: '技术教程共计',
+        value: 353,
+        unit: '篇'
+    }
+]
+
+// 主题统计TOP5
+export const data9 = {
+    series: [18, 17, 15, 13, 2],
+    axis: ['环境监测', '饲育技术', '消毒', '预防化肥', '施肥']
+}
+
+// 收藏、点赞、分享统计
+export const data10 = {
+    series: [59, 39, 2],
+    axis: ['分享用户占比', '点赞用户占比', '收藏用户占比']
+}

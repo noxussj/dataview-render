5953198aec47f43e4ac08ed7930d0634f52be5ae
HN <632922356@qq.com>
2022-09-15 16:58:22 +0800

    update

--- user-options/index.js
+++ user-options/index.js
@@ -1,38 +1,63 @@
-const { data1, data2, data3, data4, data5, data6, data7 } = require('../user-data/index.js')
+const { data1, data2, data3, data4, data5, data6, data7, data8, data9, data10 } = require('../user-data/index.js')
 
 module.exports = {
-    name: '桑蚕产业管理一张图',
-    key: 'garden',
-    prefix: 'manage',
+    name: '桑蚕产业运营一张图',
+    key: 'operate',
+    prefix: '',
     layout: [
         {
             width: '26.08%',
             height: '19.35%',
             children: [
                 {
-                    name: '气象站',
-                    key: 'base-weather',
+                    name: '小程序实时用户统计',
+                    key: 'base-applets',
                     copyData: data1
                 },
                 {
-                    name: '土壤监测',
-                    key: 'base-soil',
+                    name: '趋势分析',
+                    key: 'base-trend',
                     copyData: data2
                 },
                 {
-                    name: '本月桑园虫情预警',
-                    key: 'base-warning',
+                    name: '专家咨询服务统计',
+                    key: 'base-service-count',
                     copyData: data3
                 },
                 {
-                    name: '24小时孢子捕捉',
-                    key: 'base-spore',
+                    name: '咨询关键词统计TOP5',
+                    key: 'base-keyword',
                     copyData: data4
                 },
                 {
-                    name: '设备监测',
-                    key: 'base-devices',
+                    name: '资询趋势分析',
+                    key: 'base-service-trend',
                     copyData: data5
+                },
+                {
+                    name: '用户反馈分析',
+                    key: 'base-feedback',
+                    copyData: data6
+                },
+                {
+                    name: '政策资讯统计',
+                    key: 'base-policy',
+                    copyData: data7
+                },
+                {
+                    name: '技术教程统计',
+                    key: 'base-skill',
+                    copyData: data8
+                },
+                {
+                    name: '主题统计TOP5',
+                    key: 'base-theme',
+                    copyData: data9
+                },
+                {
+                    name: '收藏、点赞、分享统计',
+                    key: 'base-share',
+                    copyData: data10
                 }
             ]
         }
